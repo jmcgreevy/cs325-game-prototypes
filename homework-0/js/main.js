db4d9b482c0abb103c45941df05f40b46c6449a8
jmcgreev <jmcgreev@gmu.edu>
2020-02-13 02:08:00 -0500

    first game of 325

--- homework-0/js/main.js
+++ homework-0/js/main.js
@@ -1,62 +1,331 @@
-"use strict";
+var GameState = function(game) {
+	this.MAX_MISSILES = 3; // number of "missiles"
+};
 
-function make_main_game_state( game )
-{
-    function preload() {
-        // Load an image and call it 'logo'.
-        game.load.image( 'logo', 'assets/phaser.png' );
+// Load images and sounds
+GameState.prototype.preload = function() {
+    this.game.load.image('player', 'assets/sanic2.png');
+    this.game.load.image('ground', 'assets/ground.png');
+	this.game.load.image('rocket', 'assets/rocket.png');
+};
+
+// Custom game object to track player (didnt work)
+/*
+var Player = function(game, x, y) {
+	Phaser.Sprite.call(this, game, x, y, 'player');
+};
+
+Player.prototype = Object.create(Phaser.Sprite.prototype);
+
+Player.prototype.constructor = Player;
+*/
+
+var lives;
+var stateText;
+
+// Setup the example
+GameState.prototype.create = function() {
+    // Set stage background to something sky colored
+    this.game.stage.backgroundColor = 0x4488cc;
+	
+	// Lives
+	lives = game.add.group();
+	game.add.text(game.world.width - 100, 10, 'Lives : ', { font: '34px Arial', fill: '#fff' });
+
+	// Text
+	stateText = game.add.text(game.world.centerX,game.world.centerY,' ', { font: '84px Arial', fill: '#fff' });
+	stateText.anchor.setTo(0.5, 0.5);
+	stateText.visible = false;
+	
+	// this is supposed to give the player 3 lives
+	for (var i = 0; i < 3; i++) 
+    {
+        var player = lives.create(game.world.width - 100 + (30 * i), 60, 'sanic2');
+        player.anchor.setTo(0.5, 0.5);
+        player.angle = 90;
+        player.alpha = 0.4;
     }
-    
-    var bouncy;
-    
-    function create() {
-        // Create a sprite at the center of the screen using the 'logo' image.
-        bouncy = game.add.sprite( game.world.centerX, game.world.centerY, 'logo' );
-        // Anchor the sprite at its center, as opposed to its top-left corner.
-        // so it will be truly centered.
-        bouncy.anchor.setTo( 0.5, 0.5 );
-        
-        // Turn on the arcade physics engine for this sprite.
-        game.physics.enable( bouncy, Phaser.Physics.ARCADE );
-        // Make it bounce off of the world bounds.
-        bouncy.body.collideWorldBounds = true;
-        
-        // Add some text using a CSS style.
-        // Center it in X, and position its top 15 pixels from the top of the world.
-        var style = { font: "25px Verdana", fill: "#9999ff", align: "center" };
-        var text = game.add.text( game.world.centerX, 15, "Build something amazing.", style );
-        text.anchor.setTo( 0.5, 0.0 );
+
+    // Define movement constants
+    this.MAX_SPEED = 500; // pixels/second
+    this.ACCELERATION = 1500; // pixels/second/second
+    this.DRAG = 600; // pixels/second
+    this.GRAVITY = 2600; // pixels/second/second
+    this.JUMP_SPEED = -1000; // pixels/second (negative y is up)
+	
+	// Create a group to hold the missile
+    this.missileGroup = this.game.add.group();
+	
+	// Simulate a pointer click/tap input at the bottom-center of the stage
+    // when the example begins running.
+    this.game.input.activePointer.x = this.game.width/2;
+    this.game.input.activePointer.y = this.game.height/2 + 200;
+
+    // Create a player sprite
+    this.player = this.game.add.sprite(this.game.width/2, this.game.height - 200, 'player');
+
+    // Enable arcade physics on the player
+    this.game.physics.enable(this.player, Phaser.Physics.ARCADE);
+
+    // Make player collide with world boundaries so he doesn't leave the stage
+    this.player.body.collideWorldBounds = true;
+
+    // Set player minimum and maximum movement speed
+    this.player.body.maxVelocity.setTo(this.MAX_SPEED, this.MAX_SPEED * 10); // x, y
+
+    // Add drag to the player that slows them down when they are not accelerating
+    this.player.body.drag.setTo(this.DRAG, 0); // x, y
+
+    // Since we're jumping we need gravity
+    game.physics.arcade.gravity.y = this.GRAVITY;
+
+    // Create some ground for the player to walk on
+    this.ground = this.game.add.group();
+    for(var x = 0; x < this.game.width; x += 32) {
+        // Add the ground blocks, enable physics on each, make them immovable
+        var groundBlock = this.game.add.sprite(x, this.game.height - 32, 'ground');
+        this.game.physics.enable(groundBlock, Phaser.Physics.ARCADE);
+        groundBlock.body.immovable = true;
+        groundBlock.body.allowGravity = false;
+        this.ground.add(groundBlock);
     }
-    
-    function update() {
-        // Accelerate the 'logo' sprite towards the cursor,
-        // accelerating at 500 pixels/second and moving no faster than 500 pixels/second
-        // in X or Y.
-        // This function returns the rotation angle that makes it visually match its
-        // new trajectory.
-        bouncy.rotation = game.physics.arcade.accelerateToPointer( bouncy, game.input.activePointer, 500, 500, 500 );
+
+    // Capture certain keys to prevent their default actions in the browser.
+    // This is only necessary because this is an HTML5 game. Games on other
+    // platforms may not need code like this.
+    this.game.input.keyboard.addKeyCapture([
+        Phaser.Keyboard.LEFT,
+        Phaser.Keyboard.RIGHT,
+        Phaser.Keyboard.UP,
+        Phaser.Keyboard.DOWN
+    ]);
+
+    // Just for fun, draw some height markers so we can see how high we're jumping
+    this.drawHeightMarkers();
+};
+
+// This function draws horizontal lines across the stage
+GameState.prototype.drawHeightMarkers = function() {
+    // Create a bitmap the same size as the stage
+    var bitmap = this.game.add.bitmapData(this.game.width, this.game.height);
+
+    // These functions use the canvas context to draw lines using the canvas API
+    for(y = this.game.height-32; y >= 64; y -= 32) {
+        bitmap.context.beginPath();
+        bitmap.context.strokeStyle = 'rgba(255, 255, 255, 0.2)';
+        bitmap.context.moveTo(0, y);
+        bitmap.context.lineTo(this.game.width, y);
+        bitmap.context.stroke();
     }
-    
-    return { "preload": preload, "create": create, "update": update };
+
+    this.game.add.image(0, 0, bitmap);
+};
+
+// The update() method is called every frame
+GameState.prototype.update = function() {
+    // Collide the player with the ground
+    this.game.physics.arcade.collide(this.player, this.ground);
+
+    if (this.leftInputIsActive()) {
+        // If the LEFT key is down, set the player velocity to move left
+        this.player.body.acceleration.x = -this.ACCELERATION;
+    } else if (this.rightInputIsActive()) {
+        // If the RIGHT key is down, set the player velocity to move right
+        this.player.body.acceleration.x = this.ACCELERATION;
+    } else {
+        this.player.body.acceleration.x = 0;
+    }
+
+    // Set a variable that is true when the player is touching the ground
+    var onTheGround = this.player.body.touching.down;
+
+    if (onTheGround && this.upInputIsActive()) {
+        // Jump when the player is touching the ground and the up arrow is pressed
+        this.player.body.velocity.y = this.JUMP_SPEED;
+    }
+	
+	// If there are fewer than MAX_MISSILES, launch a new one
+    if (this.missileGroup.countLiving() < this.MAX_MISSILES) {
+        // Set the launch point to a random location above the stage
+        this.launchMissile(this.game.rnd.integerInRange(450, this.game.width - 150),
+            this.game.height - 600);
+    }
+	
+	// If any missile is within a certain distance of the player, blow it up
+    this.missileGroup.forEachAlive(function(m) {
+        var distance = this.game.math.distance(m.x, m.y,
+            this.player.x, this.player.y);
+        if (distance < 65) {
+            m.kill();
+        }
+    }, this);
+};
+
+// Try to get a missile from the missileGroup
+// If a missile isn't available, create a new one and add it to the group.
+GameState.prototype.launchMissile = function(x, y) {
+    // // Get the first dead missile from the missileGroup
+    var missile = this.missileGroup.getFirstDead();
+
+    // If there aren't any available, create a new one
+    if (missile === null) {
+        missile = new Missile(this.game);
+        this.missileGroup.add(missile);
+    }
+
+    // Revive the missile (set it's alive property to true)
+    missile.revive();
+
+    // Move the missile to the given coordinates
+    missile.x = x;
+    missile.y = y;
+
+    return missile;
+};
+
+
+// Missile constructor
+var Missile = function(game, x, y) {
+    Phaser.Sprite.call(this, game, x, y, 'rocket');
+
+    // Set the pivot point for this sprite to the center
+    this.anchor.setTo(0.5, 0.5);
+
+    // Enable physics on the missile
+    game.physics.enable(this, Phaser.Physics.ARCADE);
+
+    // Define constants that affect motion
+    this.SPEED = 250; // missile speed pixels/second
+    this.TURN_RATE = 5; // turn rate in degrees/frame
+};
+
+// Missiles are a type of Phaser.Sprite
+Missile.prototype = Object.create(Phaser.Sprite.prototype);
+Missile.prototype.constructor = Missile;
+
+
+Missile.prototype.update = function() {
+	
+	// If this missile is dead, don't do any of these calculations
+    // Also, turn off the smoke emitter
+    if (!this.alive) {
+        //this.smokeEmitter.on = false;
+        return;
+    } else {
+        //this.smokeEmitter.on = true;
+    }
+	
+	// track the player
+    var targetAngle = this.game.math.angleBetween(
+        this.x, this.y,
+        //this.player.position.x, this.player.position.y
+		this.game.input.activePointer.x, this.game.input.activePointer.y
+		//this.player.x, this.player.y // XXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX
+    );
+
+    // Gradually (this.TURN_RATE) aim the missile towards the target angle
+    if (this.rotation !== targetAngle) {
+        // Calculate difference between the current angle and targetAngle
+        var delta = targetAngle - this.rotation;
+
+        // Keep it in range from -180 to 180 to make the most efficient turns.
+        if (delta > Math.PI) delta -= Math.PI * 2;
+        if (delta < -Math.PI) delta += Math.PI * 2;
+
+        if (delta > 0) {
+            // Turn clockwise
+            this.angle += this.TURN_RATE;
+        } else {
+            // Turn counter-clockwise
+            this.angle -= this.TURN_RATE;
+        }
+
+        // Just set angle to target angle if they are close
+        if (Math.abs(delta) < this.game.math.degToRad(this.TURN_RATE)) {
+            this.rotation = targetAngle;
+        }
+    }
+
+    // Calculate velocity vector based on this.rotation and this.SPEED
+    this.body.velocity.x = Math.cos(this.rotation) * this.SPEED;
+    this.body.velocity.y = Math.sin(this.rotation) * this.SPEED;
+};
+
+
+// This function should return true when the player activates the "go left" control
+// In this case, either holding the right arrow or tapping or clicking on the left
+// side of the screen.
+GameState.prototype.leftInputIsActive = function() {
+    var isActive = false;
+
+    isActive = this.input.keyboard.isDown(Phaser.Keyboard.LEFT);
+    isActive |= (this.game.input.activePointer.isDown &&
+        this.game.input.activePointer.x < this.game.width/4);
+
+    return isActive;
+};
+
+// This function should return true when the player activates the "go right" control
+// In this case, either holding the right arrow or tapping or clicking on the right
+// side of the screen.
+GameState.prototype.rightInputIsActive = function() {
+    var isActive = false;
+
+    isActive = this.input.keyboard.isDown(Phaser.Keyboard.RIGHT);
+    isActive |= (this.game.input.activePointer.isDown &&
+        this.game.input.activePointer.x > this.game.width/2 + this.game.width/4);
+
+    return isActive;
+};
+
+// This function should return true when the player activates the "jump" control
+// In this case, either holding the up arrow or tapping or clicking on the center
+// part of the screen.
+GameState.prototype.upInputIsActive = function(duration) {
+    var isActive = false;
+
+    isActive = this.input.keyboard.downDuration(Phaser.Keyboard.UP, duration);
+    isActive |= (this.game.input.activePointer.justPressed(duration + 1000/60) &&
+        this.game.input.activePointer.x > this.game.width/4 &&
+        this.game.input.activePointer.x < this.game.width/2 + this.game.width/4);
+
+    return isActive;
+};
+
+function playerHit (player, rocket){
+	live = lives.getFirstAlive();
+	
+	if (live)
+    {
+        live.kill();
+    }
+	
+	if(lives.countLiving() < 1){
+		player.kill();
+		rocket.kill();
+		
+		stateText.text=" GAME OVER \n Click to restart";
+        stateText.visible = true;
+
+        //the "click to restart" handler
+        game.input.onTap.addOnce(restart,this);
+	}
+
 }
 
+function restart () {
 
-window.onload = function() {
-    // You might want to start with a template that uses GameStates:
-    //     https://github.com/photonstorm/phaser/tree/v2.6.2/resources/Project%20Templates/Basic
+    //  A new level starts
     
-    // You can copy-and-paste the code from any of the examples at http://examples.phaser.io here.
-    // You will need to change the fourth parameter to "new Phaser.Game()" from
-    // 'phaser-example' to 'game', which is the id of the HTML element where we
-    // want the game to go.
-    // The assets (and code) can be found at: https://github.com/photonstorm/phaser/tree/master/examples/assets
-    // You will need to change the paths you pass to "game.load.image()" or any other
-    // loading functions to reflect where you are putting the assets.
-    // All loading functions will typically all be found inside "preload()".
-    
-    var game = new Phaser.Game( 800, 600, Phaser.AUTO, 'game' );
-    
-    game.state.add( "main", make_main_game_state( game ) );
-    
-    game.state.start( "main" );
-};
+    //resets the life count
+    lives.callAll('revive');
+
+    //revives the player
+    player.revive();
+    //hides the text
+    stateText.visible = false;
+
+}
+
+var game = new Phaser.Game(900, 600, Phaser.AUTO, 'game');
+game.state.add('game', GameState, true);
