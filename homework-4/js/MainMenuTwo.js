15cb0ac2f3ccea819f784f18f70665db90cb5f31
jmcgreevy <41395770+jmcgreevy@users.noreply.github.com>
2020-03-05 02:30:24 -0500

    Update MainMenuTwo.js
    button tweaks

--- homework-4/js/MainMenuTwo.js
+++ homework-4/js/MainMenuTwo.js
@@ -55,12 +55,12 @@ GameStates.makeMainMenuTwo = function( game, shared ) {
 			playButton1.scale.setTo(1.2, 5);
 			
 			playButton2 = game.add.button( 240, 200, 'selectButton', optionTwo, null, 'over', 'out', 'down');
-			playButton2.scale.setTo(1, 5);
+			playButton2.scale.setTo(1.2, 5);
 			
-			playButton3 = game.add.button( 470, 200, 'selectButton', optionThree, null, 'over', 'out', 'down');
+			playButton3 = game.add.button( 480, 200, 'selectButton', optionThree, null, 'over', 'out', 'down');
 			playButton3.scale.setTo(1, 5);
 		
-			playButton4 = game.add.button( 660, 360, 'selectButton', optionFour, null, 'over', 'out', 'down');
+			playButton4 = game.add.button( 670, 360, 'selectButton', optionFour, null, 'over', 'out', 'down');
 			playButton4.scale.setTo(0.6, 2.5);
     
         },
