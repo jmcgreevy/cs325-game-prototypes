d234e00a7f562124d96f68a7220efe87b97192f4
jmcgreevy <41395770+jmcgreevy@users.noreply.github.com>
2020-03-05 02:36:26 -0500

    Update MainMenu.js

--- homework-4/js/MainMenu.js
+++ homework-4/js/MainMenu.js
@@ -23,8 +23,8 @@ GameStates.makeMainMenu = function( game, shared ) {
             //	Here all we're doing is playing some music and adding a picture and button
             //	Naturally I expect you to do something significantly better :)
     
-            music = game.add.audio('titleMusic');
-            music.play();
+            //music = game.add.audio('titleMusic');
+            //music.play();
     
             game.add.sprite(0, 0, 'titlePage');
     
