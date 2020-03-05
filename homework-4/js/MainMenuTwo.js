135f5208686fea3d9fa5d5b9a081734810723ef4
jmcgreevy <41395770+jmcgreevy@users.noreply.github.com>
2020-03-04 23:35:16 -0500

    Update MainMenuTwo.js
    change button placement

--- homework-4/js/MainMenuTwo.js
+++ homework-4/js/MainMenuTwo.js
@@ -28,11 +28,11 @@ GameStates.makeMainMenuTwo = function( game, shared ) {
     
             game.add.sprite(0, 0, 'titlePage2');
     
-            playButton = game.add.button( 300, 100, 'playButton', startGame, null, 'over', 'out', 'down');
+            playButton = game.add.button( 100, 400, 'playButton', startGame, null, 'over', 'out', 'down');
 			
-			playButton = game.add.button( 300, 300, 'playButton', startGame, null, 'over', 'out', 'down');
+			playButton = game.add.button( 200, 400, 'playButton', startGame, null, 'over', 'out', 'down');
 			
-			playButton = game.add.button( 300, 500, 'playButton', startGame, null, 'over', 'out', 'down');
+			playButton = game.add.button( 300, 400, 'playButton', startGame, null, 'over', 'out', 'down');
     
         },
     
